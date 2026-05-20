e6d1194c3349433e0f2d21b1dca23287c01391a8
Ivan Sharganov <sharganov.ivan@gmail.com>
2026-05-20 12:43:04 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Ivan Sharganov on 20.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
